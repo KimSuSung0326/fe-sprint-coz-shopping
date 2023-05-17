491381dbde27e6560771d4766322ad0d6f89f037
KimSuSung0326 <suseong326@naver.com>
2023-05-17 16:44:16 +0900

    Footer 기능 구현

---
+++ Solo_ShoppingMall_VersionTwo/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './base.css';
+const Footer = () =>{
+    return(
+        <div>
+            <hr/>
+            <footer className="footer">
+                개인정보 처리방침| 이용 약관 <br/>All rights reserved &copy; Codestates
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
